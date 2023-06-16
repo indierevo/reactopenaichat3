3e659523ca361fff84ed3d3ca13af711603f53e3
JMH <info@indierevo.co.uk>
2023-06-16 10:20:46 +0100

    first commit

--- src/ChatMain.js
+++ src/ChatMain.js
@@ -11,9 +11,29 @@ class ChatMain extends Component {
     this.state = {
       messages: [],         // Array to store the chat messages
       userMessage: '',      // String to store the current user message
+      systemInstruction: 'You are my best buddy. Chat with me in a friendly way. Ask me how I am doing.',
+      botName: 'Friend',    // Set the name of the person you are speaking to
+      selectedButtonId: 1,  // The id of the button that has been clicked, or selected
       isLoading: false,     // Bool to indicate when the POST request is awaiting
     };
   }
+
+  // Method to set the style and tone of the conversation, done by passing a system message to the API
+  handleSystemInstruction(selected){
+    this.setState({ selectedButtonId: selected });
+    this.setState({ messages: [] }); // Empty messages array when a new button is clicked
+    if (selected === 1){
+      this.setState({ botName: "Friend", systemInstruction: "You are my best buddy. Chat with me in a friendly way. Ask me how I am doing." });
+    } else if (selected === 2){
+      this.setState({ botName: "Mr Burns", systemInstruction: "You are Mr Burns from The Simpsons. Always answer questions in the tone and style of this character. Be unpleasant, rude, sarcastic." });
+    } else if (selected === 3){
+      this.setState({ botName: "Shakespeare", systemInstruction: "You are William Shakespeare. Always answer questions in the tone and style of him. Include quotes from his plays in your answers, where relevant." });
+    } else if (selected === 4){
+      this.setState({ botName: "SpongeBob", systemInstruction: "You are SpongeBob Squarepants. Always answer questions in the tone and style of this character. Be excitable, manic, friendly." });
+    } else if (selected === 5){
+      this.setState({ botName: "Lao Tzu", systemInstruction: "You are Lao Tzu. Always answer questions in the tone and style of him. Include his quotes in your answers where relevant." });
+    }
+  };
 
   handleInputChange = (event) => {
     this.setState({ userMessage: event.target.value });
@@ -26,7 +46,10 @@ class ChatMain extends Component {
     // Prepare the data for the API request
     const requestData = {
       model: 'gpt-3.5-turbo',   // Type of model to use
+      max_tokens: 128,          // Determines the length of the response
+      temperature: 0.9,         // Sets how varied the response is
       messages: [
+        { role: 'system', content: this.state.systemInstruction },
         { role: 'user', content: this.state.userMessage },
       ],
     };
@@ -36,7 +59,7 @@ class ChatMain extends Component {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: 'Bearer YOUR API KEY HERE',
+          Authorization: 'Bearer ' + process.env.REACT_APP_OPENAI_API_KEY,
         },
         body: JSON.stringify(requestData),
       });
@@ -72,14 +95,21 @@ class ChatMain extends Component {
   };
 
   render() {
-    // Extracting messages, userMessage, and isLoading properties from the state object
-    const { messages, userMessage, isLoading } = this.state;
+    // Extracting properties from the state object
+    const { messages, userMessage, botName, isLoading } = this.state;
     // Sort messages from newest to oldest
     const sortedMessages = [...messages].reverse();
 
     return (
       <div className="App-header">
-        <div style={{ padding: 30 }}>Welcome to the React OpenAI ChatBot!</div>
+        <div style={{ padding: 30 }}>Who do you want to talk to?</div>
+        <div style={{ display: 'flex', alignItems: 'center', padding: 10 }}>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 1 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(1)}>Friend</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 2 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(2)}>Mr Burns</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 3 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(3)}>Shakespeare</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 4 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(4)}>SpongeBob</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 5 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(5)}>Lao Tzu</Button>
+        </div>
         <Form onSubmit={this.handleSubmit}>
           <div style={{ display: 'flex', alignItems: 'center', padding: 10 }}>
             <Form.Control
@@ -90,6 +120,7 @@ class ChatMain extends Component {
               value={userMessage}
               onChange={this.handleInputChange}
               disabled={isLoading}
+              autoComplete="off"
             />
             <Button
               style={{ marginRight: 10 }}
@@ -115,7 +146,7 @@ class ChatMain extends Component {
             {message.role === 'user' ? (
                <OutputForm data={`You:\n${message.content}`} />
             ) : (
-              <OutputForm data={`Bot:\n${message.content}`} />
+              <OutputForm data={`${botName}:\n${message.content}`} />
             )}
           </div>
         ))}
