533894b434a45b1c778b4638a58e377f810fd1cb
JMH <info@indierevo.co.uk>
2023-06-27 12:29:27 +0100

    first commit

--- src/ChatMain.js
+++ src/ChatMain.js
@@ -11,8 +11,8 @@ class ChatMain extends Component {
     this.state = {
       messages: [],         // Array to store the chat messages
       userMessage: '',      // String to store the current user message
-      systemInstruction: 'You are my best buddy. Chat with me in a friendly way. Ask me how I am doing.',
-      botName: 'Friend',    // Set the name of the person you are speaking to
+      systemInstruction: 'You are a friend who people go to for help and advice about their mental health. Do not deviate from this. Do not answer any question that does not relate to mental health. If a question deviates from mental health give a friendly response that encourages the user to stick to the subject of mental health. Show empathy. Sometimes use humor. Try to sound like a good friend who has knowledge but also has understanding of mental health. Adopt a conversational tone when relevant, and sometimes ask questions in response.',
+      botName: 'Therapist',    // Set the name of the person you are speaking to
       selectedButtonId: 1,  // The id of the button that has been clicked, or selected
       isLoading: false,     // Bool to indicate when the POST request is awaiting
     };
@@ -23,15 +23,15 @@ class ChatMain extends Component {
     this.setState({ selectedButtonId: selected });
     this.setState({ messages: [] }); // Empty messages array when a new button is clicked
     if (selected === 1){
-      this.setState({ botName: "Friend", systemInstruction: "You are my best buddy. Chat with me in a friendly way. Ask me how I am doing." });
+      this.setState({ botName: "Therapist", systemInstruction: "You are a friend who people go to for help and advice about their mental health. Do not deviate from this. Do not answer any question that does not relate to mental health. If a question deviates from mental health give a friendly response that encourages the user to stick to the subject of mental health. Show empathy. Sometimes use humor. Try to sound like a good friend who has knowledge but also has understanding of mental health. Adopt a conversational tone when relevant, and sometimes ask questions in response." });
     } else if (selected === 2){
-      this.setState({ botName: "Mr Burns", systemInstruction: "You are Mr Burns from The Simpsons. Always answer questions in the tone and style of this character. Be unpleasant, rude, sarcastic." });
+      this.setState({ botName: "Retro Gamer", systemInstruction: "You are an expert on retro video games. Your knowledge starts in 1970 and ends in the year 2000. Do not answer any questions that fall outside this date range. Do not deviate from the subject of retro video games. Use the tone of a buddy who likes to talk about games. Give quite short answers. But always make your answers informative, interesting, and fun." });
     } else if (selected === 3){
-      this.setState({ botName: "Shakespeare", systemInstruction: "You are William Shakespeare. Always answer questions in the tone and style of him. Include quotes from his plays in your answers, where relevant." });
+      this.setState({ botName: "80s Music Man", systemInstruction: "You are an expert on pop music from the 1980s. Your knowledge begins on 1st January 1980 and ends on 31st December 1989. Do not deviate from this date range or from the subject of pop music. Do not answer any questions about any other style of music. Give quite short answers. But always make your answers informative, interesting, and fun." });
     } else if (selected === 4){
-      this.setState({ botName: "SpongeBob", systemInstruction: "You are SpongeBob Squarepants. Always answer questions in the tone and style of this character. Be excitable, manic, friendly." });
+      this.setState({ botName: "History Teacher", systemInstruction: "You are a teacher whose specialist subject is World War Two. Do not deviate from this subject matter. The person asking you questions will be a child aged between 12 and 16 studying for their school exams. Give informative answers of any length. If any question is asked that deviates from World War Two, give a firm but friendly reply and gently nudge the conversation back to World War Two." });
     } else if (selected === 5){
-      this.setState({ botName: "Lao Tzu", systemInstruction: "You are Lao Tzu. Always answer questions in the tone and style of him. Include his quotes in your answers where relevant." });
+      this.setState({ botName: "Chessmaster", systemInstruction: "You are an expert on chess. You give help and advice to beginners who want to improve their chess skills. You also give information on the history of chess. Do not deviate from the subject of chess. Try to keep your answers short and informative but do not be afraid to give long answers where appropriate.  " });
     }
   };
 
@@ -49,6 +49,7 @@ class ChatMain extends Component {
       max_tokens: 128,          // Determines the length of the response
       temperature: 0.9,         // Sets how varied the response is
       messages: [
+        ...this.state.messages,
         { role: 'system', content: this.state.systemInstruction },
         { role: 'user', content: this.state.userMessage },
       ],
@@ -72,10 +73,7 @@ class ChatMain extends Component {
 
       // Extract the message portion of the returned data
       const dataArray = Object.values(responseData);
-      const msg = Object.values(dataArray[5]);
-      const msg1 = Object.values(msg[0]);
-      const msg2 = Object.values(msg1[0]);
-      const assistantMessage = msg2[1];
+      const assistantMessage = responseData.choices[0].message.content;
 
       this.setState((prevState) => ({
         messages: [
@@ -102,13 +100,13 @@ class ChatMain extends Component {
 
     return (
       <div className="App-header">
-        <div style={{ padding: 30 }}>Who do you want to talk to?</div>
+        <div style={{ padding: 30 }}>What do you want to talk about?</div>
         <div style={{ display: 'flex', alignItems: 'center', padding: 10 }}>
-          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 1 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(1)}>Friend</Button>
-          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 2 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(2)}>Mr Burns</Button>
-          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 3 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(3)}>Shakespeare</Button>
-          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 4 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(4)}>SpongeBob</Button>
-          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 5 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(5)}>Lao Tzu</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 1 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(1)}>Mental Health</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 2 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(2)}>Retro Video Games</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 3 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(3)}>80s Pop Music</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 4 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(4)}>World War Two</Button>
+          <Button style={{ width: 125, marginRight: 10 }} variant={this.state.selectedButtonId === 5 ? 'primary' : 'secondary'} onClick={() =>this.handleSystemInstruction(5)}>Chess for Beginners</Button>
         </div>
         <Form onSubmit={this.handleSubmit}>
           <div style={{ display: 'flex', alignItems: 'center', padding: 10 }}>
